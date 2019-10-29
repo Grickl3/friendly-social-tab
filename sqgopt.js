32b165784b1ee35d68a995945d3d4321528e7d43
DeaconRodda <deacon@sqglz.com>
2019-10-28 20:15:50 -0600

    cloned over sqgopt contents as a starting point

---
+++ sqgopt.js
@@ -0,0 +1,30 @@
+
+
+const sqgopt = function() {
+	console.log("When this project is developed there will be no more excuse for aggressive permission gathering elements on landing pages.")
+};
+
+
+const sqgoptSetTrigger = function() {
+	ctaBox = sqgOptCTABox.getBoundingClientRect();
+	sqgOptTrigger1.style.marginTop = ctaBox.top + "px";
+	sqgOptPostTrigger.style.marginTop = "-" + ctaBox.top + "px";
+};
+
+// const sqgoptSetModal = function() {
+// 	sqgOptTrigger.addEventListener("click", function {
+// 		sqgOptModal.classList.remove("hidden");
+// 	});
+// 	sqgOptCloseX.addEventListener("click", function {
+// 		sqgOptModal.classList.add("hidden");
+// 	});
+// 	sqgOptScreen.addEventListener("click", function {
+// 		sqgOptModal.classList.add("hidden");
+// 	});
+// }
+
+const sqgoptFindCTA = function() {
+
+	console.log(ctaBox.top);
+
+};
